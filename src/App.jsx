a6aaaf3f35479db60058ad44811010c79fbee121
Thananon Chounudom <thananonchounudom@Thananons-MacBook-Pro.local>
2026-01-16 15:52:38 +0700

    Setup core styles and routing structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
+import SalesJournal from './pages/SalesJournal';
+
+function App() {
+  return (
+    <HashRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/journal" element={<SalesJournal />} />
+        </Routes>
+      </Layout>
+    </HashRouter>
+  );
+}
+
+export default App;
